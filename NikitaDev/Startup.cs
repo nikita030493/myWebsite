d57a3fee75ea0f7d2b1ef5efee8eab808a6af739
Nikita Uryupin <nikita030493@live.com>
2018-10-19 10:09:16 -0400

    Removed unused references

--- NikitaDev/Startup.cs
+++ NikitaDev/Startup.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace NikitaDev
